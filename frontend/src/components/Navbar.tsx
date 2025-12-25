ab0f63bca98ae97dc21a437e5050dba00feba323
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2025-12-25 02:09:36 +0100

    Added pictures and fixed video

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,20 +7,20 @@ export default function Navbar() {
   return (
     <>
       <nav
-        className={`navbar ${hovered ? "navbar--active" : ""} flex flex-row justify-between`}
+        className={`navbar ${hovered ? "bg-white border-t-2  border-b-2 border-black border-solid transition ease-in-out transform-[3s] " : "bg-transparent"} flex flex-row justify-between`}
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
       >
-        <div className="navbar-left  flex flex-row"  >
-          <button className="button cursor-pointer  flex flex-row">
+        <div className="navbar-left  flex flex-row ml-5 mt-5 gap-3.75"  >
+          <button className="button cursor-pointer  flex flex-row ">
             <Menu /> Menu
           </button>
           <button className="button cursor-pointer flex flex-row">
             <Search /> Search
           </button>
         </div>
-        <div>Anonimowi Alkoholicy</div>
-        <div className="navbar-right flex flex-row">
+        <div className="font-bold text-[40px]">ANONYMOUS</div>
+        <div className="navbar-right flex flex-row mr-5  gap-3.75">
           <button className="cursor-pointer">Contact us </button>
           <button><UserRound className="cursor-pointer"/></button>
           <button><ShoppingBasket className="cursor-pointer"/></button>
