66c4e8fafbed8ef68e8a8f3d4d320889642149fc
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2025-12-28 23:08:21 +0100

    Add useState to input

--- frontend/src/Search.tsx
+++ frontend/src/Search.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Search() {
+    const [text,setText] = useState("");
+
+
 
     return (
         <>
@@ -8,7 +12,7 @@ export default function Search() {
         <Link to="/">
         <div className="font-bold text-[40px]  flex justify-center items-center">ANONYMOUS</div></Link>
         <div className=" flex justify-center items-center">
-        <input type="text" placeholder="  Search for clothes"className=" flex justify-center items-center w-[900px] border-[1px] rounded-[20px]"></input>
+        <input type="text" value={text} onChange={(e)=> {setText(e.target.value)}} placeholder="  Search for clothes"className=" flex justify-center items-center w-[900px] border-[1px] rounded-[20px]"></input>
         </div>
         
         
