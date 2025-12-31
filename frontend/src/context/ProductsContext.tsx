6b9977841c25290d294d2d47f8559819fc015145
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2026-01-01 00:38:41 +0100

    Add fetch and filter

--- frontend/src/context/ProductsContext.tsx
+++ frontend/src/context/ProductsContext.tsx
@@ -1,30 +1,42 @@
-import { Children, createContext, useState } from "react";
+import { Children, createContext, useEffect, useState } from "react";
 
 export type Product = {
-    id: number;
-    title: string;
-    price: number;
-    category: string;
-    image: string;
-    rating: {rate: number}
-}
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  image: string;
+  rating: { rate: number };
+};
 
 type Filters = {
-    search: string;
-    category: string;
-    minPrice: number;
-    maxPrice: number;
-}
+  search: string;
+  category: string;
+  minPrice: number;
+  maxPrice: number;
+};
 
 const ProductsContext = createContext<any>(null);
 
-export const ProductsProvider = ({Children}: any) => {
-    const [products,setProducts] = useState<Product[]>([]);
-    const [filters,setFilters] = useState<Filters>({
-        search: "",
-        category: "all",
-        minPrice: 0, 
-        maxPrice: 10000
-    })
+export const ProductsProvider = ({ Children }: any) => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [filters, setFilters] = useState<Filters>({
+    search: "",
+    category: "all",
+    minPrice: 0,
+    maxPrice: 10000,
+  });
 
-}
+  useEffect(() => {
+    fetch("https://fakestoreapiserver.reactbd.org/api/products")
+        .then(res => res.json())
+        .then(setProducts);
+    },[]);
+
+    const filteredProducts = products.filter(p => {
+        p.title.toLowerCase().includes(filters.search.toLowerCase()) &&
+        (filters.category === "all" || p.category === filters.category) && 
+        (p.price >= filters.minPrice && p.price <= filters.maxPrice)
+    })
+};
+
