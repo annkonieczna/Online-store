363c9ac9995dd4d0836d14e7a09e0154fa564cfe
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2026-01-01 03:04:54 +0100

    Change the location of ProductProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ProductsProvider } from "./context/ProductsContext";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ProductsProvider>
     <App />
-    </ProductsProvider>
+    
   </StrictMode>
 );
