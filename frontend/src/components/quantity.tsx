24172e2d2ec19ee7b97231e804dfd225905e1857
Anna Konieczna <annkonieczna@student.agh.edu.pl>
2026-01-03 02:18:23 +0100

    Switch + and - sides

--- frontend/src/components/quantity.tsx
+++ frontend/src/components/quantity.tsx
@@ -11,16 +11,6 @@ const Quantity = ({ max }: { max: number }) => {
       }}
     >
       <button
-        style={{ width: "2vw", height: "2vw" }}
-        className="accept-bt"
-        onClick={() => {
-          if (quantity < max) setQuan((prev) => prev + 1);
-        }}
-      >
-        +
-      </button>
-      <p style={{ width: "1.5vw", textAlign: "center" }}>{quantity}</p>
-      <button
         style={{ width: "2vw", height: "2vw", position: "sticky" }}
         className="accept-bt"
         onClick={() => {
@@ -28,6 +18,16 @@ const Quantity = ({ max }: { max: number }) => {
         }}
       >
         -
+      </button>
+      <p style={{ width: "1.5vw", textAlign: "center" }}>{quantity}</p>
+      <button
+        style={{ width: "2vw", height: "2vw" }}
+        className="accept-bt"
+        onClick={() => {
+          if (quantity < max) setQuan((prev) => prev + 1);
+        }}
+      >
+        +
       </button>
     </div>
   );
